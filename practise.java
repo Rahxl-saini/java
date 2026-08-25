a138292f2979f5d1bada1db0fbd3359eb17c8630
Rahul Saini <rahulsaini2522@gmail.com>
2026-08-26 01:15:23 +0530

    update

---
+++ practise.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class practise {
+    public static void main(String[] args){
+        int arr[] = {2, 7, 11, 15};
+        int target = 13;
+
+        boolean found = false;
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i] + arr[j] == target){
+                    System.out.println("pairs exists: " + arr[i] + " + " + arr[j]);
+                    found = true;
+                }
+            }
+        }
+        if(!found){
+            System.out.println("pairs not exists");
+        }
+    }
+}
