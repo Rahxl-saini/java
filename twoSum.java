206b402cda56d70f94d452e9c92eedbcb160e0ca
Rahul Saini <rahulsaini2522@gmail.com>
2026-08-30 15:06:12 +0530

    update

---
+++ twoSum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class twoSum{
+    public static void main(String[] args) {
+
+        int[] arr = {2, 7, 11, 15};
+        int target = 13;
+
+        boolean found = false;
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] + arr[j] == target) {
+                    System.out.println("Pair exists: " 
+                            + arr[i] + " + " + arr[j]);
+                    found = true;
+                }
+            }
+        }
+
+        if (!found) {
+            System.out.println("Pair does not exist");
+        }
+    }
+}
